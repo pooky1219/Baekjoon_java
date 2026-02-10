b0c2ee60759de3a6495cbfdd3bf044d2ffd006cf
Yonghoon Lee <88873780+pooky1219@users.noreply.github.com>
2026-02-10 14:06:55 +0900

    [Bronze V] Title: Hello World, Time: 84 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader는 입력 스트림을 버퍼링하여 읽기 속도를 높입니다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Hello World!");
+    }
+}

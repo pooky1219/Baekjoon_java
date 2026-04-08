200bc2e71b2eddf1e476a88ef7b6c2c5e67c2b8f
Yonghoon Lee <88873780+pooky1219@users.noreply.github.com>
2026-04-08 18:45:21 +0900

    [level 1] Title: 폰켓몬, Time: 7.25 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,13 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        //홍박사님을 아세요? 홍홍홍
-        Map<Integer,Integer>map = new HashMap<>();
-        for(int n : nums){
-            map.put(n,map.getOrDefault(n,0)+1);
+        Map<Integer, Integer> pokemons = new HashMap<>();
+        // 포켓몬 입력
+        for(int pokemon : nums) {
+            int num = pokemons.getOrDefault(pokemon, 0);
+            pokemons.put(pokemon, num + 1);
         }
-        int size = map.size();
-        int answer = Math.min(nums.length/2,size);
+        
+        int answer = Math.min(pokemons.size(), nums.length / 2);
+            
         return answer;
     }
 }
